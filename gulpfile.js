0c03a7fb62f20e8278457c80b7ade24556159047
Pontus Ellboj <pellboj@Pontuss-MBP.lan>
2017-06-18 19:03:55 +0200

    add ability to clean dist folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 const eslint = require('gulp-eslint')
+const clean = require('gulp-clean')
 
 gulp.task('default', function () {
   // Run ESLint
-  gulp.src(['es6/**/*.js', 'public/es6/**/*.js'])
+  gulp.src(['src/**/*.js', 'public/src/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
   // Node source
-  gulp.src('es6/**/*.js')
+  gulp.src('src/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'))
   // browser source
-  gulp.src('public/es6/**/*.js')
+  gulp.src('public/src/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('public/dist'))
+})
+// clean dist folders
+gulp.task('clean', function () {
+  gulp.src('dist/*.js', {read: false})
+    .pipe(clean())
+  gulp.src('public/dist/*.js', {read: false})
+    .pipe(clean())
 })
